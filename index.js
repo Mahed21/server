38b705a323fcb20af3ab10f60312b63d05f6e501
Mahed21 <mahed255261@gmail.com>
2022-02-10 19:23:00 +0600

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { MongoClient } = require('mongodb');
 const app = express()
 const cors=require('cors');
+const ObjectId=require('mongodb').ObjectId;
 const port = process.env.PORT || 5000
 require('dotenv').config()
 
@@ -25,6 +26,15 @@ async function run() {
        const cursor=userCollection.find ({})
        const users=await cursor.toArray();
        res.send(users);
+     })
+
+     //get single details of rent
+     app.get('/homes/:id', async (req,res)=>
+     {
+       const id =req.params.id;
+       const query={_id: ObjectId(id)};
+       const user =await userCollection.findOne(query);
+       res.send(user);
      })
     
   
